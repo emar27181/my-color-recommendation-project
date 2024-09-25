1b66c03ae7578833d3b7c33349d57a940e2fdfd2
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-25 17:41:34 +0900

    ベースカラーを基に色を推薦する機能の初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -7,10 +7,23 @@ def read_file(file_path):
         return data
 
 
+def generate_recommend_colors(base_color):
+
+    colors = [
+        base_color,
+        (0, 0, 0),
+        (255, 255, 255)
+    ]
+    return colors
+
+
 def main():
     FILE_PATH = "src/color_recommendation/data/input/test_input.json"
     data = read_file(FILE_PATH)
     print(f'data:', data)
+
+    colors = generate_recommend_colors((255, 0, 0))
+    print(f'colors:', colors)
 
 
 if __name__ == '__main__':
