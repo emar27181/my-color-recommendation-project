369c28d1c2813b2d2cf116fd545731c3c714c555
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-26 20:04:07 +0900

    引数で受け取った配列(色相差)を基に配色を生成する関数の初期実装

--- src/color_recommendation/utils/generate_color_scheme_method.py
+++ src/color_recommendation/utils/generate_color_scheme_method.py
@@ -1,7 +1,7 @@
 from transform_color import rgb_to_hsl, hsl_to_rgb, rgb_to_hex, hex_to_rgb
 
 
-def generate_dominant_color_sheme(base_color_rgb):
+def generate_color_scheme(base_color_rgb, hue_differences):
     base_color_hsl = (rgb_to_hsl(base_color_rgb))
     base_color_hue = base_color_hsl[0]
     base_color_saturation = base_color_hsl[1]
@@ -9,16 +9,21 @@ def generate_dominant_color_sheme(base_color_rgb):
 
     color_scheme = []
     color_scheme.append(base_color_rgb)
-    color_scheme.append(hsl_to_rgb((base_color_hue + 30) % 360, base_color_saturation, base_color_lightness))
-    color_scheme.append(hsl_to_rgb((base_color_hue + 60) % 360, base_color_saturation, base_color_lightness))
 
+    for hue_difference in hue_differences:
+        color_scheme.append(hsl_to_rgb((base_color_hue + hue_difference) % 360, base_color_saturation, base_color_lightness))
+    return color_scheme
+
+
+def generate_dominant_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [30, 60])
     return color_scheme
 
 
 def main():
-    print(generate_dominant_color_sheme([255, 0, 0]))
-    print(generate_dominant_color_sheme([0, 255, 0]))
-    print(generate_dominant_color_sheme([0, 0, 0]))
+    print(generate_dominant_color_scheme([255, 0, 0]))
+    print(generate_dominant_color_scheme([0, 255, 0]))
+    print(generate_dominant_color_scheme([0, 0, 0]))
 
     print(-50 % 360)
     print(50 % 360)
