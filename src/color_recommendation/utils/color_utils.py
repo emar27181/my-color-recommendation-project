577d7be062910539e2a6738ae454e04f36070473
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-29 17:25:51 +0900

    引数で受け取ったRGB値の文字を表示させる関数の実装

---
+++ src/color_recommendation/utils/color_utils.py
@@ -0,0 +1,7 @@
+# 引数で受け取ったRGB値の文字を表示させる関数
+def print_colored_text(text, rgb):
+    # RGBから16進数カラーコードに変換
+    hex_color = '#{:02x}{:02x}{:02x}'.format(rgb[0], rgb[1], rgb[2])
+
+    # ANSIエスケープシーケンスを使って色を設定
+    print(f"\033[38;2;{rgb[0]};{rgb[1]};{rgb[2]}m{text}\033[0m", end="")
