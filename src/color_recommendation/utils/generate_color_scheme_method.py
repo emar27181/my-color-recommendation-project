b69ec69bc8ce2d989374d20456a4a16691fc0849
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-29 18:01:02 +0900

    引数で受け取ったベースカラーを基にした配色技法をまとめて返す関数の実装

--- src/color_recommendation/utils/generate_color_scheme_method.py
+++ src/color_recommendation/utils/generate_color_scheme_method.py
@@ -2,6 +2,30 @@ from transform_color import rgb_to_hsl, hsl_to_rgb, rgb_to_hex, hex_to_rgb
 from color_utils import print_colored_text, print_color_schemes
 
 
+def generate_all_color_schemes(base_color_rgb):
+    color_schemes = []
+
+    color_schemes.append(generate_identity_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_intermediate_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_dyad_color_scheme(base_color_rgb))
+    color_schemes.append(generate_analogy_color_scheme(base_color_rgb))
+    color_schemes.append(generate_oponent_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_dominant_color_scheme(base_color_rgb))
+    color_schemes.append(generate_split_complementary_color_scheme(base_color_rgb))
+    color_schemes.append(generate_triad_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_tetrad_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_pentad_color_scheme(base_color_rgb))
+
+    color_schemes.append(generate_hexad_color_scheme(base_color_rgb))
+
+    return color_schemes
+
+
 def generate_color_scheme(base_color_rgb, hue_differences):
     base_color_hsl = (rgb_to_hsl(base_color_rgb))
     base_color_hue = base_color_hsl[0]
@@ -19,7 +43,7 @@ def generate_color_scheme(base_color_rgb, hue_differences):
 # 1色
 def generate_identity_color_scheme(base_color_rgb):
     color_scheme = generate_color_scheme(base_color_rgb, [])
-    return color_scheme
+    return color_scheme,
 
 
 # 2色
@@ -37,7 +61,7 @@ def generate_intermediate_color_scheme(base_color_rgb):
 
 def generate_dyad_color_scheme(base_color_rgb):
     color_scheme = generate_color_scheme(base_color_rgb, [180])
-    return color_scheme
+    return color_scheme,
 
 
 def generate_oponent_color_scheme(base_color_rgb):
@@ -61,22 +85,27 @@ def generate_split_complementary_color_scheme(base_color_rgb):
     return color_scheme1, color_scheme2, color_scheme3
 
 
+def generate_triad_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [120, 240])
+    return color_scheme,
+
+
 # 4色
 def generate_tetrad_color_scheme(base_color_rgb):
     color_scheme = generate_color_scheme(base_color_rgb, [90, 180, 270])
-    return color_scheme
+    return color_scheme,
 
 
 # 5色
 def generate_pentad_color_scheme(base_color_rgb):
     color_scheme = generate_color_scheme(base_color_rgb, [72, 144, 216, 288])
-    return color_scheme
+    return color_scheme,
 
 
 # 6色
 def generate_hexad_color_scheme(base_color_rgb):
     color_scheme = generate_color_scheme(base_color_rgb, [60, 120, 180, 240, 300])
-    return color_scheme
+    return color_scheme,
 
 
 def main():
@@ -84,7 +113,10 @@ def main():
     recommend_color_schemes.append(generate_analogy_color_scheme([255, 0, 0]))
     recommend_color_schemes.append(generate_dominant_color_scheme([255, 0, 0]))
 
-    print_color_schemes(recommend_color_schemes)
+    # print_color_schemes(recommend_color_schemes)
+
+    color_schemes = generate_all_color_schemes([255, 0, 0])
+    print_color_schemes(color_schemes)
 
 
 if __name__ == '__main__':
