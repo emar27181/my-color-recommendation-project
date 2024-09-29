02e0bcbaba75d3150f97d4673db2c9b312454865
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-29 17:02:25 +0900

    1~6色の配色技法を追加する関数をそれぞれ実装

--- src/color_recommendation/utils/generate_color_scheme_method.py
+++ src/color_recommendation/utils/generate_color_scheme_method.py
@@ -15,6 +15,37 @@ def generate_color_scheme(base_color_rgb, hue_differences):
     return color_scheme
 
 
+# 1色
+def generate_identity_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [])
+    return color_scheme
+
+
+# 2色
+def generate_analogy_color_scheme(base_color_rgb):
+    color_scheme1 = generate_color_scheme(base_color_rgb, [30])
+    color_scheme2 = generate_color_scheme(base_color_rgb, [-30])
+    return color_scheme1, color_scheme2
+
+
+def generate_intermediate_color_scheme(base_color_rgb):
+    color_scheme1 = generate_color_scheme(base_color_rgb, [90])
+    color_scheme2 = generate_color_scheme(base_color_rgb, [-90])
+    return color_scheme1, color_scheme2
+
+
+def generate_dyad_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [180])
+    return color_scheme
+
+
+def generate_oponent_color_scheme(base_color_rgb):
+    color_scheme1 = generate_color_scheme(base_color_rgb, [150])
+    color_scheme2 = generate_color_scheme(base_color_rgb, [-150])
+    return color_scheme1, color_scheme2
+
+
+# 3色
 def generate_dominant_color_scheme(base_color_rgb):
     color_scheme1 = generate_color_scheme(base_color_rgb, [30, 60])
     color_scheme2 = generate_color_scheme(base_color_rgb, [-30, 30])
@@ -22,15 +53,34 @@ def generate_dominant_color_scheme(base_color_rgb):
     return color_scheme1, color_scheme2, color_scheme3
 
 
+def generate_split_complementary_color_scheme(base_color_rgb):
+    color_scheme1 = generate_color_scheme(base_color_rgb, [150, -150])
+    color_scheme2 = generate_color_scheme(base_color_rgb, [-150, 60])
+    color_scheme3 = generate_color_scheme(base_color_rgb, [-60, 150])
+    return color_scheme1, color_scheme2, color_scheme3
+
+
+# 4色
+def generate_tetrad_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [90, 180, 270])
+    return color_scheme
+
+
+# 5色
+def generate_pentad_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [72, 144, 216, 288])
+    return color_scheme
+
+
+# 6色
+def generate_hexad_color_scheme(base_color_rgb):
+    color_scheme = generate_color_scheme(base_color_rgb, [60, 120, 180, 240, 300])
+    return color_scheme
+
 
 def main():
     print(generate_dominant_color_scheme([255, 0, 0]))
-    print(generate_dominant_color_scheme([0, 255, 0]))
-    print(generate_dominant_color_scheme([0, 0, 0]))
-
-    print(-50 % 360)
-    print(50 % 360)
-    print((0 - 60) % 360)
+    print(generate_analogy_color_scheme([255, 0, 0]))
 
 
 if __name__ == '__main__':
