84357557b35ec8199b0bcc2645be7e1930d7d1e7
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-09-30 19:23:38 +0900

    入力したイラストデータの色情報の取得の初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -1,4 +1,7 @@
 import json
+from utils.generate_color_scheme_method import generate_all_color_schemes
+from utils.helpers.transform_color import hex_to_rgb
+from utils.helpers.color_utils import print_colored_text
 
 
 def read_file(file_path):
@@ -7,23 +10,28 @@ def read_file(file_path):
         return data
 
 
-def generate_recommend_colors(base_color):
+def generate_recommend_colors(data):
 
-    colors = [
-        base_color,
-        (0, 0, 0),
-        (255, 255, 255)
-    ]
-    return colors
+    # print(data)
+    for illust_data in data:
+        print(illust_data)
+        for color_data in illust_data:
+            color = color_data['color']
+            print_colored_text("■ ", hex_to_rgb(color))
+            print(color)
+
+    return ("test output")
 
 
 def main():
-    FILE_PATH = "src/color_recommendation/data/input/test_input.json"
+    FILE_PATH = "src/color_recommendation/data/input/test_input_simple_data.json"
     data = read_file(FILE_PATH)
-    print(f'data:', data)
+    # print(f'data:', data)
 
-    colors = generate_recommend_colors((255, 0, 0))
-    print(f'colors:', colors)
+    generate_recommend_colors(data)
+
+    # colors = generate_recommend_colors((255, 0, 0))
+    # print(f'colors:', colors)
 
 
 if __name__ == '__main__':
