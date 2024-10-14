5a144c4a7f995f62d473303011458d99b149f620
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-10-14 12:10:57 +0900

    次の色が含まれているかどうかの情報を表示させるかどうかを保存する固定値の実装

--- src/color_recommendation/utils/check_data_is_contained_next_color.py
+++ src/color_recommendation/utils/check_data_is_contained_next_color.py
@@ -1,25 +1,30 @@
 from utils.helpers.color_utils import calculate_color_difference_delta_e_cie2000, print_colored_text
 from utils.helpers.transform_color import hex_to_rgb
 
+IS_PRINT_CONTAINED_NEXT_COLOR_INFO = False
+
 
 def is_contained_color(next_color, color_schemes):
     for i in range(len(color_schemes)):
         # print(color_schemes[i])
-        print(f"--- {i+1} 配色目 -------------")
+
+        if (IS_PRINT_CONTAINED_NEXT_COLOR_INFO):
+            print(f"--- {i+1} 配色目 -------------")
 
         for j in range(len(color_schemes[i])):
             recommend_color = color_schemes[i][j]["color"]
             # print(recommend_color)
 
-            print("next_color: ", end="")
-            print_colored_text("■ ", hex_to_rgb(next_color))
-            print(", recommend_color: ", end="")
-            print_colored_text("■", hex_to_rgb(recommend_color))
-
             diff = calculate_color_difference_delta_e_cie2000(hex_to_rgb(next_color), hex_to_rgb(recommend_color))
-            print(f", delta_e = {diff}")
 
-            if (diff <= 40):
+            if (IS_PRINT_CONTAINED_NEXT_COLOR_INFO):
+                print("next_color: ", end="")
+                print_colored_text("■ ", hex_to_rgb(next_color))
+                print(", recommend_color: ", end="")
+                print_colored_text("■", hex_to_rgb(recommend_color))
+                print(f", delta_e = {diff}")
+
+            if (diff <= 20):
                 return True, i
 
     return False, -1
@@ -38,7 +43,9 @@ def check_data_is_contained_next_color(data):
 
         # 次の色が含まれているかどうかの判定
         for i in range(len(color_scheme) - 1):
-            print(f"\n=== {illust_count+1} 枚目のイラストの {i+1} 色目 ============")
+
+            if (IS_PRINT_CONTAINED_NEXY_COLOR_INFO):
+                print(f"\n=== {illust_count+1} 枚目のイラストの {i+1} 色目 ============")
 
             # corrent_color = color_scheme[i]["color"]
             next_color = color_scheme[i + 1]["color"]
