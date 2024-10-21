414209290bb5d07c5d2daa4419e984a438ff0182
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-10-21 12:21:32 +0900

    recall@kのグラフを生成する機能の初期実装

--- src/color_recommendation/utils/plot_graph.py
+++ src/color_recommendation/utils/plot_graph.py
@@ -1,9 +1,49 @@
-def plot_recall_at_k():
-    print("this is plot recall at k")
+import json
+import matplotlib.pyplot as plt
+
+
+def plot_recall_at_k(file_path):
+    recalls = [0] * 100
+    timing_count = 0
+
+    with open(file_path, 'r') as f:
+        data = json.load(f)
+
+    print(len(data))
+
+    for illust_data in data:
+        # print(illust_data['recall_at_k'])
+
+        for illust_data_at_timing in illust_data['recall_at_k']:
+            timing_count += 1
+            # print(illust_data_at_timing)
+            if (illust_data_at_timing['is_contained_next_color']):
+                # print(illust_data_at_timing["k"])
+                for i in range(illust_data_at_timing["k"], len(recalls)):
+                    recalls[i] += 1
+
+    for i in range(len(recalls)):
+        # print(recalls[i])
+        recalls[i] = round(100 * (recalls[i] / timing_count)) / 100
+
+    print(timing_count)
+    print(recalls)
+
+    # プロット
+    plt.plot(recalls, marker='o')
+    plt.title('Recalls Plot')
+    plt.ylim(0, 1)
+    plt.xlim(0, 60)
+    plt.xlabel('Index')
+    plt.ylabel('Recall Values')
+    plt.grid(True)
+
+    # ファイルに保存
+    plt.savefig('src/color_recommendation/data/output/test_recall_at_k.png')
 
 
 def main():
-    plot_recall_at_k()
+    plot_recall_at_k("src/color_recommendation/data/output/test_is_contained_next_color_simple_data.json")
 
 
 if __name__ == '__main__':
