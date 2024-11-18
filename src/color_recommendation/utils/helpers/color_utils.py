d962a3f266531b89fd257cd50c32bb6a34760ed8
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-11-18 11:39:57 +0900

    引数で受け取った配色のうちΔE値が閾値以下の色を結合する関数の変数名をより明示的な名前に変更

--- src/color_recommendation/utils/helpers/color_utils.py
+++ src/color_recommendation/utils/helpers/color_utils.py
@@ -5,30 +5,30 @@ from PIL import Image
 import numpy as np
 
 
-def merge_similar_color_counts(color_scheme, threshold):
+def merge_similar_color_counts(color_counts, same_color_threshold):
     """引数で受け取った配色のうちΔE値が閾値以下の色を結合する関数"""
-    merged_colors = []  # 結合する色を保存する配列を初期化
+    merged_color_counts = []  # 結合する色を保存する配列を初期化
 
     # 配色がある限り色を結合
-    while len(color_scheme) > 0:
-        base_color = color_scheme[0]  # 先頭の色をベースカラーに代入
-        to_merge = [base_color]
-        color_scheme = color_scheme[1:]
+    while len(color_counts) > 0:
+        base_color_count = color_counts[0]  # 先頭の色をベースカラーに代入
+        to_merge = [base_color_count]
+        color_counts = color_counts[1:]
 
         # 先頭の色と先頭以外の色でΔEが閾値以下の場合があるかどうかの探索
-        for i in range(len(color_scheme) - 1, -1, -1):  # 最後尾([len(~)-1])からから先頭([-1])まで検索
+        for i in range(len(color_counts) - 1, -1, -1):  # 最後尾([len(~)-1])からから先頭([-1])まで検索
 
             # 先頭の色とi番目の色が同じ色だった場合
-            if calculate_color_difference_delta_e_cie2000(base_color[0], color_scheme[i][0]) <= threshold:
-                to_merge.append(color_scheme[i])  # 結合する色を保存するスロットにi番目の色を追加
-                color_scheme.pop(i)  # 結合されるi番目の色を削除
+            if calculate_color_difference_delta_e_cie2000(base_color_count[0], color_counts[i][0]) <= same_color_threshold:
+                to_merge.append(color_counts[i])  # 結合する色を保存するスロットにi番目の色を追加
+                color_counts.pop(i)  # 結合されるi番目の色を削除
 
         # 色の加重平均と合算された出現回数を色を結合する配列に追加
         merge_color_total_count = sum([color[1] for color in to_merge])  # 出現回数の合算
         merge_color_rgb = np.average([color[0] for color in to_merge], axis=0, weights=[color[1] for color in to_merge])
         merge_color_rgb = np.round(merge_color_rgb).astype(int)
-        merged_colors.append([merge_color_rgb, merge_color_total_count])
-    return merged_colors
+        merged_color_counts.append([merge_color_rgb, merge_color_total_count])
+    return merged_color_counts
 
 
 def calculate_dict_value_sum(dict):
