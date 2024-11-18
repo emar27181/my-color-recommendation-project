316e2b5a2c20094420eebce9c32f5f75e2a020ce
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2024-11-18 11:26:35 +0900

    引数で受け取った配色のうちΔE値が閾値以下の色を結合する関数のコピー実装

--- src/color_recommendation/utils/helpers/color_utils.py
+++ src/color_recommendation/utils/helpers/color_utils.py
@@ -5,6 +5,32 @@ from PIL import Image
 import numpy as np
 
 
+def merge_similar_color_counts(color_scheme, threshold):
+    """引数で受け取った配色のうちΔE値が閾値以下の色を結合する関数"""
+    merged_colors = []  # 結合する色を保存する配列を初期化
+
+    # 配色がある限り色を結合
+    while len(color_scheme) > 0:
+        base_color = color_scheme[0]  # 先頭の色をベースカラーに代入
+        to_merge = [base_color]
+        color_scheme = color_scheme[1:]
+
+        # 先頭の色と先頭以外の色でΔEが閾値以下の場合があるかどうかの探索
+        for i in range(len(color_scheme) - 1, -1, -1):  # 最後尾([len(~)-1])からから先頭([-1])まで検索
+
+            # 先頭の色とi番目の色が同じ色だった場合
+            if calculate_color_difference_delta_e_cie2000(base_color[0], color_scheme[i][0]) <= threshold:
+                to_merge.append(color_scheme[i])  # 結合する色を保存するスロットにi番目の色を追加
+                color_scheme.pop(i)  # 結合されるi番目の色を削除
+
+        # 色の加重平均と合算された出現回数を色を結合する配列に追加
+        merge_color_total_count = sum([color[1] for color in to_merge])  # 出現回数の合算
+        merge_color_rgb = np.average([color[0] for color in to_merge], axis=0, weights=[color[1] for color in to_merge])
+        merge_color_rgb = np.round(merge_color_rgb).astype(int)
+        merged_colors.append([merge_color_rgb, merge_color_total_count])
+    return merged_colors
+
+
 def calculate_dict_value_sum(dict):
     """引数で受け取った辞書型の配列のvalueの合計値を計算する関数"""
     sum = 0
