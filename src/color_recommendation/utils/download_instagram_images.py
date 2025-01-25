7519b5fc1e4a0716c244c88de041bf159af81a1f
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-01-25 15:50:33 +0900

    引数で受け取るリスト内のイラストレーターのイラストを一括ダウンロードする関数の実装

--- src/color_recommendation/utils/download_instagram_images.py
+++ src/color_recommendation/utils/download_instagram_images.py
@@ -1,6 +1,22 @@
 import instaloader
 
 
+def download_instagram_images_for_illustrators(illustrator_list, illust_count_limit):
+    """
+    引数で受け取るリスト内のイラストレーターのイラストを一括ダウンロードする関数
+
+    引数:
+        illustrator_list: イラストレーターのリスト(文字列)
+        illust_count_limit: イラストのダウンロード数の限界値
+    戻り値:
+        None
+    """
+    for illustrator_name in illustrator_list:
+        print(f"=== {illustrator_name} ====================")
+        download_instagram_images(illustrator_name, illust_count_limit)
+        print(f"@{illustrator_name} のイラストがダウンロードされました．")
+
+
 # 引数で受け取ったユーザ名のイラストを一括ダウンロードする関数
 def download_instagram_images(profile_name, illust_count_limit):
     # Instaloaderインスタンスを作成
