dce9a0d402b99b0b477a991465aba2ee8db65a22
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-04-12 01:50:08 +0900

    引数で受け取った配色群を平均結果長にソートする関数(<=引数で受け取った配色群に特定の色相差が含まれているか判定する関数)の実装

--- src/color_recommendation/utils/sort_color_scheme.py
+++ src/color_recommendation/utils/sort_color_scheme.py
@@ -1,4 +1,4 @@
-from utils.helpers.color_utils import print_color_scheme, calc_color_scheme_difference_delta_e_cie2000, print_color_schemes
+from utils.helpers.color_utils import print_color_scheme, calc_color_scheme_difference_delta_e_cie2000, print_color_schemes, calc_pccs_color_diff
 import random
 from utils.helpers.json_utils import get_json_data
 from utils.analyze_illustrator_statistics import get_statistics_by_illustrator
@@ -39,6 +39,30 @@ def sort_color_scheme_by_color_difference(base_color_scheme, color_schemes):
     return color_schemes
 
 
+def _is_included_pccs_diff_in_color_scheme(color_scheme, pccs_diff):
+    """引数で受け取った配色群に特定の色相差が含まれているか判定する関数
+    引数:
+        color_scheme: 配色群
+        pccs_diff: 判定するpccs_diff
+
+    戻り値:
+        True: 含まれている
+        False: 含まれていない
+    """
+
+    pccs_diffs = calc_pccs_color_diff(color_scheme[0], color_scheme)
+
+    if pccs_diff in pccs_diffs:
+        if (DEBUG):
+            print(f"True (pccs_diff: {pccs_diff}, pccs_diffs: {pccs_diffs})")
+        return True
+
+    else:
+        if (DEBUG):
+            print(f"False (pccs_diff: {pccs_diff}, pccs_diffs: {pccs_diffs})")
+        return False
+
+
 def sort_color_schemes_by_mean_resultant_length(color_schemes, illustrator_name):
     """引数で受け取った配色群を平均結果長にソートする関数
     引数:
@@ -48,15 +72,29 @@ def sort_color_schemes_by_mean_resultant_length(color_schemes, illustrator_name)
         color_schemes: 色差の昇順にソートされた配色群
 
     """
+    new_color_schemes = []
+
     mean_resultant_length_distribution = get_statistics_by_illustrator(illustrator_name, "mean_resultant_length_distribution")
     print(f"mean_resultant_length_distribution: {mean_resultant_length_distribution}")
 
-    sorted_indices = sorted(range(len(mean_resultant_length_distribution)),
-                            key=lambda i: mean_resultant_length_distribution[i],
-                            reverse=True)  # 値で降順に並べたときのインデックスを取得
-    print(sorted_indices)
+    pccs_sorted_used_count = sorted(range(len(mean_resultant_length_distribution)),
+                                    key=lambda i: mean_resultant_length_distribution[i],
+                                    reverse=True)  # 値で降順に並べたときのインデックスを取得
 
-    return color_schemes
+    """
+    for color_scheme in color_schemes:
+        calc_pccs_color_diff(color_scheme[0], color_scheme)
+    """
+
+    print(f"pccs_sorted_use_count {pccs_sorted_used_count}")
+
+    for pccs in pccs_sorted_used_count:
+        for color_scheme in color_schemes:
+            if _is_included_pccs_diff_in_color_scheme(color_scheme, pccs):
+                new_color_schemes.append(color_scheme)
+                color_schemes.remove(color_scheme)
+
+    return new_color_schemes
 
 
 def _get_analysis_data(illustrator_name):
