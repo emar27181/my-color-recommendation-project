024a32a3b17778d0261bfbd25ae8002162db80ba
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-04-18 19:26:10 +0900

    同じ色相かどうかでスコアを計算する関数の仮実装

--- src/color_recommendation/utils/helpers/calc_rec_score.py
+++ src/color_recommendation/utils/helpers/calc_rec_score.py
@@ -4,15 +4,18 @@
 
 from utils.helpers.color_utils import print_color_scheme
 
+DEBUG = False
+
 
 def calc_same_hue_score(used_color_scheme, rec_color_shceme):
     """ 同じ色相かどうかでスコアを計算する関数
     """
 
-    print("======================")
-    print("used_color_scheme: ", end="")
-    print_color_scheme(used_color_scheme)
-    print("rec_color_scheme:  ", end="")
-    print_color_scheme(rec_color_shceme)
+    if (DEBUG):
+        print("======================")
+        print("used_color_scheme: ", end="")
+        print_color_scheme(used_color_scheme)
+        print("rec_color_scheme:  ", end="")
+        print_color_scheme(rec_color_shceme)
 
     return 1
