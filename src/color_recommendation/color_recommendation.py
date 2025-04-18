9364fc05d040ea525c5771defb4353d79a8cd00d
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-04-18 20:58:53 +0900

    推薦色相を保存する処理の初期実装

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -3,7 +3,7 @@ from utils.generate_color_scheme_method import generate_all_color_schemes, remov
 from utils.add_variations_color_scheme import get_lightness_variations_color_schemes
 from utils.helpers.color_utils import print_colored_text, print_color_schemes, print_color_scheme
 from utils.helpers.transform_color import hex_to_rgb, transform_color_schemes_rgb_to_hex
-from utils.helpers.json_utils import convert_color_schemes_to_color_data
+from utils.helpers.json_utils import convert_color_schemes_to_color_data, save_json_data
 from utils.check_data_is_contained_next_color import check_data_is_contained_next_color
 
 from utils.estimate_used_color_scheme import generate_json_used_color_scheme, save_estimated_used_colors
@@ -22,15 +22,41 @@ def read_file(file_path):
         return data
 
 
-def generate_recommend_colors(data, sort_type, illustrator_name, lightness_diffs):
+def generate_recommend_hues(data, sort_type, illustrator_name):
 
     output_data = []
-
-    # print(data)
     for illust_data in data:
 
         # 空のデータを読み飛ばし
         if not illust_data:
+            continue
+
+        print(f"=== {illust_data[0]['illustName']} =================== ")
+        # 使用配色のRGB形式のリストを取得
+        used_color_scheme_rgb = []
+        for color_scheme_data in illust_data:
+            used_color_scheme_rgb.append(hex_to_rgb(color_scheme_data['color']))
+
+        # あるイラストに対して推薦配色群を生成
+        base_color_rgb = hex_to_rgb(illust_data[0]['color'])  # 推薦配色の基となる色を取得
+        recommend_color_schemes_rgb = generate_all_color_schemes(base_color_rgb)  # 17(?)パターンの配色群を生成
+
+        new_illust_data = {
+            "illust_name": illust_data[0]['illustName'],
+            "color_scheme": illust_data,
+            "recommend_color_schemes": convert_color_schemes_to_color_data(transform_color_schemes_rgb_to_hex(recommend_color_schemes_rgb)),
+        }
+
+        output_data.append(new_illust_data)
+
+    return output_data
+
+
+def generate_recommend_colors(data, sort_type, illustrator_name, lightness_diffs):
+
+    output_data = []
+    for illust_data in data:
+        if not illust_data:  # 空のデータを読み飛ばし
             continue
 
         print(f"=== {illust_data[0]['illustName']} =================== ")
@@ -44,12 +70,15 @@ def generate_recommend_colors(data, sort_type, illustrator_name, lightness_diffs
         base_color_rgb = hex_to_rgb(illust_data[0]['color'])  # 推薦配色の基となる色を取得
         recommend_base_color_schemes_rgb = generate_all_color_schemes(base_color_rgb)  # 17(?)パターンの配色群を生成
         recommend_color_schemes_rgb = recommend_base_color_schemes_rgb.copy()
-        # recommend_color_schemes_rgb = remove_duplicated_color_from_color_schemes(recommend_color_schemes_rgb) # 重複色を削除
+
+        # 重複色を削除
+        # recommend_color_schemes_rgb = remove_duplicated_color_from_color_schemes(recommend_color_schemes_rgb)
 
         # 生成された推薦配色群に明度彩度が異なる色を追加
         for lightness_diff in lightness_diffs:
             recommend_color_schemes_rgb += get_lightness_variations_color_schemes(recommend_base_color_schemes_rgb, lightness_diff)
 
+        # 推薦配色群の並び替え
         if (sort_type == "color_diff"):
             recommend_color_schemes_rgb = sort_color_scheme_by_color_difference(used_color_scheme_rgb, recommend_color_schemes_rgb)  # 使用配色との類似度順にソート
         elif (sort_type == "random"):
@@ -78,10 +107,7 @@ def generate_recommend_colors(data, sort_type, illustrator_name, lightness_diffs
                 print(f"[{i}]: ", end="")
                 print_color_scheme(recommend_color_schemes_rgb[i])
 
-        # print(new_illust_data)
         output_data.append(new_illust_data)
-
-    # print(output_data)
 
     return output_data
 
@@ -102,20 +128,28 @@ def save_recommend_colors_for_illustrators(illutrator_list, sort_type, lightness
         print(f"=== {illustrator_name} ====================")
 
         input_file_path = f"src/color_recommendation/data/input/used_colors/used_colors_{illustrator_name}.json"
+        output_dir_path_hues = f"src/color_recommendation/data/output/recommend_hues/sort_by_{sort_type}"
+        output_file_path_hues = f"src/color_recommendation/data/output/recommend_hues/sort_by_{sort_type}/recommend_hues_{illustrator_name}.json"
+        output_dir_path_colors = f"src/color_recommendation/data/output/recommend_colors/sort_by_{sort_type}"
+        output_file_path_colors = f"src/color_recommendation/data/output/recommend_colors/sort_by_{sort_type}/recommend_colors_{illustrator_name}.json"
         used_colors_data = read_file(input_file_path)
 
+        # 推薦配色群の生成
+        recommend_hues_data = generate_recommend_hues(used_colors_data, sort_type, illustrator_name)
         recommend_colors_data = generate_recommend_colors(used_colors_data, sort_type, illustrator_name, lightness_diffs)
-        # 推薦配色の生成と推薦配色のソートを別処理にした方が分かりやすいかも
-        output_file_path = f"src/color_recommendation/data/output/recommend_colors/sort_by_{sort_type}/recommend_colors_{illustrator_name}.json"
 
-        if not os.path.exists(os.path.dirname(output_file_path)):
-            output_dir_path = f"src/color_recommendation/data/output/recommend_colors/sort_by_{sort_type}"
+        # 推薦配色群の保存
+        save_json_data(recommend_hues_data, output_dir_path_hues, output_file_path_hues)
+        save_json_data(recommend_colors_data, output_dir_path_colors, output_file_path_colors)
+        """
+        if not os.path.exists(os.path.dirname(output_file_path_colors)):
             os.makedirs(output_dir_path)
             print(f"{output_dir_path} ディレクトリが作成されました．")
 
-        with open(output_file_path, 'w', encoding='utf-8') as file:
+        with open(output_file_path_colors, 'w', encoding='utf-8') as file:
             json.dump(recommend_colors_data, file, ensure_ascii=False, indent=4)
-            print(f"{output_file_path} が保存されました．(推薦配色群の生成)")
+            print(f"{output_file_path_colors} が保存されました．(推薦配色群の生成)")
+        """
 
 
 def main():
