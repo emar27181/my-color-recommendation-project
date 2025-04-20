0ba8a82dfd940749d8c0e66ba3c4249c3b2a26c3
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-04-20 16:33:03 +0900

    次の色が含まれているかどうかを判断するファイルのリファクタリング

--- src/color_recommendation/utils/check_data_is_contained_next_color.py
+++ src/color_recommendation/utils/check_data_is_contained_next_color.py
@@ -3,23 +3,21 @@ from utils.helpers.transform_color import hex_to_rgb
 import json
 import os
 
-IS_PRINT_CONTAINED_NEXT_COLOR_INFO = False
+DEBUG = False
 
 
 def is_contained_color(next_color, color_schemes):
     for i in range(len(color_schemes)):
-        # print(color_schemes[i])
 
-        if (IS_PRINT_CONTAINED_NEXT_COLOR_INFO):
+        if (DEBUG):
             print(f"--- {i+1} 配色目 -------------")
 
         for j in range(len(color_schemes[i])):
             recommend_color = color_schemes[i][j]["color"]
-            # print(recommend_color)
 
             diff = calculate_color_difference_delta_e_cie2000(hex_to_rgb(next_color), hex_to_rgb(recommend_color))
 
-            if (IS_PRINT_CONTAINED_NEXT_COLOR_INFO):
+            if (DEBUG):
                 print("next_color: ", end="")
                 print_colored_text("■ ", hex_to_rgb(next_color))
                 print(", recommend_color: ", end="")
@@ -53,8 +51,6 @@ def save_data_is_contained_next_color_for_illustrators(illutrator_list, sort_typ
 
         is_contained_next_color_data = check_data_is_contained_next_color(recommended_colors_data)
 
-        # print(f"{is_contained_next_color_data}")
-
         output_file_path = f"src/color_recommendation/data/output/is_contained_next_color/{sort_type}/is_contained_next_color_{illutrator_name}.json"
 
         if not os.path.exists(os.path.dirname(output_file_path)):
@@ -85,16 +81,15 @@ def check_data_is_contained_next_color(data):
         # 次の色が含まれているかどうかの判定
         for i in range(len(color_scheme) - 1):
 
-            if (IS_PRINT_CONTAINED_NEXT_COLOR_INFO):
+            if (DEBUG):
                 print(f"\n=== {illust_count+1} 枚目のイラストの {i+1} 色目 ============")
 
-            # corrent_color = color_scheme[i]["color"]
             next_color = color_scheme[i + 1]["color"]
 
-            # print(f"corrent_color: {corrent_color}, next_color: {next_color}")
+            # 次の色が含まれているかどうかを判定
             is_contained, scheme_index = is_contained_color(next_color, recommend_color_schemes)
-            # print(f"is_contained: {is_contained}, scheme_index: {scheme_index}")
 
+            # recall@kのデータの作成
             is_contained_data = {
                 "next_color_index": i + 1,
                 "k": scheme_index,
@@ -102,16 +97,12 @@ def check_data_is_contained_next_color(data):
             }
             recall_at_k_per_illust.append(is_contained_data)
 
-        # print(recall_at_k_per_illust)
-
         data_recall_at_k_per_illust = {
             "illust_name": illust_data["illust_name"],
             "recall_at_k": recall_at_k_per_illust,
         }
         data_recall_at_k.append(data_recall_at_k_per_illust)
 
-        # print(illust_data)
         illust_count += 1
 
-    # print(data_recall_at_k)
     return data_recall_at_k
