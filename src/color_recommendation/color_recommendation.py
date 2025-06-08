124a54d3dacb12b7c93134f4a40d864681e8b21a
ema_ryu-note-pc <ee207008@meiji.ac.jp>
2025-06-08 21:10:12 +0900

    既存のカラーパレットを基に色を推薦するのアプリ名を変更できるように修正

--- src/color_recommendation/color_recommendation.py
+++ src/color_recommendation/color_recommendation.py
@@ -113,9 +113,14 @@ def generate_recommend_hues_exising_apps_by_illustrator(data, sort_type, illustr
     Returns:
         _type_: _description_
     """
+    app_name = "clipstudio"  
+    app_name = "ibispaint"  
+    app_name = "medibang"
+    app_name = "pixivsketch"
+    app_name = "powerpoint"
 
     # 仮実装として，clipstudioの配色を使用
-    input_file_path = "src/color_recommendation/data/output/recommend_colors/sort_by_illust_app/recommend_colors_clipstudio.json"
+    input_file_path = f"src/color_recommendation/data/output/recommend_colors/sort_by_illust_app/recommend_colors_{app_name}.json"
     json_data = get_json_data(input_file_path)
     recommend_color_schemes_data = json_data[0]['recommend_color_schemes']
     # print(f"recommend_color_schemes_data_re: {recommend_color_schemes_data_re}")
@@ -288,7 +293,6 @@ def save_recommendations_for_illustrators(illutrator_list, recommend_type, sort_
             recommend_colors_data = generate_recommend_colors_by_illustrator(used_colors_data, sort_type, illustrator_name, lightness_diffs)
             save_json_data(recommend_colors_data, output_dir_path_colors, output_file_path_colors)
         elif recommend_type == "hue_existing_apps":
-            print("hue_existing_appsは実装中です(2025/06/08)")
             recommend_hues_existing_apps_data = generate_recommend_hues_exising_apps_by_illustrator(used_colors_data, sort_type, illustrator_name)
             
             output_dir_path = f"src/color_recommendation/data/output/recommend_{recommend_type}s/sort_by_{sort_type}"
